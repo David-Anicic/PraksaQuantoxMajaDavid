4b1eab7823e785c520b8c16c3e8b09b2261d4318
David Anicic <david.anicic369@gmail.com>
2018-07-23 16:00:41 +0200

    Dodata LISTA svih korisnika pored table za igranje.Testiranje rute na Backend-u

--- IgreOnline/src/app/board/board.component.ts
+++ IgreOnline/src/app/board/board.component.ts
@@ -1,45 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { User } from 'user';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent implements OnInit
-{
+export class BoardComponent implements OnInit {
+
   values: number[] = [0,1,2];
   srcImages: number[][] = [[0],[0],[0],[0],[0],[0],[0],[0]];
   m: string[][] = [["blank.png"],["blank.png"],["blank.png"],["blank.png"],["blank.png"],["blank.png"],["blank.png"],["blank.png"]];
   player: number;
   gameOver: boolean;
+  users: User[] = [];
+  selectedUser: User;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit()
-  {
+  ngOnInit() {
     this.player = 1;
     this.gameOver = false;
-    for (let i = 0; i < 3; i++)
-    {
-      for (let j = 0; j < 3; j++)
-      {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
           this.srcImages[i][j] = 0; 
           this.m[i][j] = "blank.png";
       }
     }
+
+    this.getAllUsers().subscribe(odgovor => {
+      console.log(odgovor['data']);
+      this.users = odgovor['data'];
+      //console.log(this.users);
+    });
   }
 
-  clicked(x:number, y:number): void
-  {
+  clicked(x:number, y:number): void {
     if (this.srcImages[x][y] == 0)
-    if (this.player == 1)
-    {
+    if (this.player == 1) {
       this.srcImages[x][y] = 1;
       this.m[x][y] = "x.png";
       this.player = 0;
-    }
-    else
-    {
+    } else {
       this.srcImages[x][y] = 2;
       this.player = 1;
       this.m[x][y] = "o.png";
@@ -48,12 +53,23 @@ export class BoardComponent implements OnInit
     this.checkStateOfTheMatrix();
   }
 
-  checkStateOfTheMatrix(): void
-  {
+  getAllUsers(): Observable<User[]> {
+    const bearerHeader: string = 'Bearer ' + '';
+    let headers = new HttpHeaders().set('Authorization', bearerHeader);
+    
+    let url = environment.serverUrl + 'users';
+    console.log(url);
+    let formData = new FormData();
+
+		//formData.append('username', "");
+
+		return this.http.get<User[]>(url);
+  }
+
+  checkStateOfTheMatrix(): void {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        if (this.srcImages[i][j] == 0)
-        {
+        if (this.srcImages[i][j] == 0) {
           return;
         }
       }
--- IgreOnline/src/environments/environment.ts
+++ IgreOnline/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:8080/Backend-master'
+  serverUrl: 'http://tictactoe.local:8000/api/'
 };
 
 /*
